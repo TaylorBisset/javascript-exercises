f8c8fbec29b88d37b4ae0afa9c5ab1e3aa81abdb
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-09-20 12:56:13 -0700

    02_repeatString complete. all tests passed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function(string, repeatNum) {
     let result = '';
+    if (repeatNum < 0) {
+        return 'ERROR'
+    }
     for (let i = 0; i < repeatNum; i++) {
         result += string;
     }
